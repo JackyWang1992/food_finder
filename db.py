9d09d7f6f5c02ebcced278c3c3e4a0c8c984dd1f
JiaqiWang <wangjiaqi1992@outlook.com>
2019-04-26 20:47:12 -0400

    version->right

---
+++ db.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sqlite3
+import json
+import pandas as pd
+import re
+import io
+ 
+def load_data():
+   
+    if os.path.exists("yelp.db"):
+        os.remove("yelp.db")
+
+    # next, we will create a new SQLite database.
+    # create a database connection.
+    conn = sqlite3.connect("yelp.db")
+    # create a cursor (this is like a single session)
+    curr = conn.cursor()
+    # send a pragma command to tell SQLite to check foreign key
+    # constraints (it does not by default :( )
+    curr.execute("PRAGMA foreign_keys = ON;")
+
+
+    # create tables.
+    curr.execute("CREATE TABLE BUSINESS (address TEXT ,attributes TEXT, business_id TEXT PRIMARY KEY, categories TEXT, city TEXT, hours TEXT, is_open INTEGER, latitude FLOAT , longitude FLOAT ,name TEXT,postal_code TEXT,review_count INTEGER ,stars FLOAT ,state TEXT)")
+    curr.execute("CREATE TABLE REVIEW (business_id TEXT, cool INTEGER , date TEXT, funny INTEGER , review_id TEXT, stars INTEGER, review TEXT, useful INTEGER , user_id TEXT)")
+    curr.execute("CREATE TABLE TIP (tip TEXT, date TEXT, compliment_count INTEGER, business_id TEXT, user_id TEXT)")
+
+    # commit is like save -- if you don't do it, nothing is written.
+    conn.commit()
+
+    # json files are too large for memory, so read them chunk by chunk using panda reader
+    buffer_size = io.DEFAULT_BUFFER_SIZE
+    # to read business.json
+    business_reader = pd.read_json("business.json", lines=True,chunksize=buffer_size)
+    buffer = next(business_reader)
+    while True:
+        # convert it from dataframe to list of list
+        business_t = list(map(list, buffer.values))
+        for i in business_t:
+            # for dictionary object, conver them to string
+            i[1] = str(i[1]) if isinstance(i[1], dict) else "None"
+            i[3] = str(i[3]) if isinstance(i[3], dict) else "None"
+            i[5] = str(i[5]) if isinstance(i[5], dict) else "None"
+        # insert into the table
+        curr.executemany("INSERT INTO BUSINESS (address,attributes, business_id, categories, city, hours, is_open, latitude, longitude, name ,postal_code,review_count,stars,state) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?);", business_t)
+        # commit change
+        conn.commit()
+        # to stop the while loop
+        try:
+            buffer = next(business_reader)
+        except:
+            break
+    business_reader.close()
+    print("finish business")
+    # read tip.json
+    tip_reader = pd.read_json("tip.json", lines=True, chunksize=buffer_size, encoding='utf-8')
+    buffer = next(tip_reader)
+    while True:
+        tip_t = list(map(list, buffer.values))
+        for i in tip_t:
+            i[2] = str(i[2]) if isinstance(i[2], pd._libs.tslibs.timestamps.Timestamp) else "None"
+            i[3] = re.sub(r'[^\x00-\x7f]', '', i[3])
+        curr.executemany("INSERT INTO TIP (business_id,compliment_count, date, tip, user_id ) VALUES (?,?,?, ?,?);",tip_t)
+        conn.commit()
+        try:
+            buffer = next(tip_reader)
+        except:
+            break
+    tip_reader.close()
+    print("finish tip")
+    # read review.json
+    review_reader = pd.read_json("review.json", lines=True, chunksize=buffer_size)
+    buffer = next(review_reader)
+    while True:
+        review_t = list(map(list, buffer.values))
+        for i in review_t:
+            i[2] = str(i[2]) if isinstance(i[2], pd._libs.tslibs.timestamps.Timestamp) else "None"
+            i[6] = re.sub(r'[^\x00-\x7f]', '', i[6])
+        curr.executemany("INSERT INTO REVIEW (business_id, cool, date, funny, review_id, stars, review, useful, user_id) VALUES (?,?,?, ?,?,?, ?,?,?);",review_t)
+        conn.commit()
+        try:
+            buffer = next(review_reader)
+        except:
+            break
+    review_reader.close()
+    print("finish review")
+
+    conn.close()
+
+def merge():
+    result = open("result.json", 'w')
+    conn = sqlite3.connect("yelp1.db")
+    curr = conn.cursor()
+    # BUSINESS + REVIEW
+    curr.execute(
+        "SELECT BUSINESS.business_id,BUSINESS.name, BUSINESS.address, BUSINESS.attributes, BUSINESS.categories, BUSINESS.city, BUSINESS.hours, BUSINESS.postal_code ,BUSINESS.review_count ,BUSINESS.stars ,BUSINESS.state,"
+        "REVIEW.review,REVIEW.useful, REVIEW.cool, REVIEW.funny, REVIEW.date"
+        " FROM BUSINESS, REVIEW"
+        " WHERE BUSINESS.business_id = REVIEW.business_id "
+        " AND BUSINESS.state = 'CA'")
+    # BUSINESS + TIP
+    # curr.execute(
+    #     "SELECT BUSINESS.business_id,BUSINESS.name, BUSINESS.address, BUSINESS.attributes, BUSINESS.categories, BUSINESS.city, BUSINESS.hours, BUSINESS.postal_code ,BUSINESS.review_count ,BUSINESS.stars ,BUSINESS.state,"
+    #     "TIP.tip, TIP.date, TIP.compliment_count"
+    #     " FROM BUSINESS, TIP"
+    #     " WHERE BUSINESS.business_id = TIP.business_id "
+    #     " AND BUSINESS.state = 'CA'")
+    # print(curr)
+    # write to json
+    final = {}
+    i = 0
+    # convert to dict
+    for content in curr.fetchall():
+        dd = {}
+        dd['business_id'] = content[0]
+        dd['business_name'] = content[1]
+        dd['address'] = content[2]
+        dd['attributes'] = content[3]
+        dd['categories'] = content[4]
+        dd['city'] = content[5]
+        dd['hours'] = content[6]
+        dd['postal_code'] = content[7]
+        dd['review_count'] = content[8]
+        dd['stars'] = content[9]
+        dd['state'] = content[10]
+        dd['review'] = content[11]
+        dd['useful'] = content[12]
+        dd['cool'] = content[13]
+        dd['funny'] = content[14]
+        dd['date'] = content[15]
+        final[i] = dd
+        i += 1
+
+    json.dump(final, result)
+    result.close()
+
+
+if __name__ == "__main__":
+    # load_data()
+    merge()
--- query.py
+++ query.py
@@ -1,101 +1,329 @@
 """
-query.py
-Dependencies: python 3.x, flask
+This module implements a (partial, sample) query interface for elasticsearch movie search. 
+You will need to rewrite and expand sections to support the types of queries over the fields in your UI.
 
-Students: Modify this code to provide an interface for your Boolean search engine
-To start the application:
-   >python query.py
-To terminate the application, use control-c
-To use the application within a browser, use the url:
-   http://127.0.0.1:5000/
+Documentation for elasticsearch query DSL:
+https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl.html
 
-Some test queries to exercise the dummy interface:
-king of sweden
-<next button>
-prince
-a of
+For python version of DSL:
+https://elasticsearch-dsl.readthedocs.io/en/latest/
 
-To learn flask, see flask tutorial in https://www.tutorialspoint.com/flask/index.htm
+Search DSL:
+https://elasticsearch-dsl.readthedocs.io/en/latest/search_dsl.html
 """
 
-from flask import Flask, render_template, request
-from search import dummy_search, dummy_movie_data, dummy_movie_snippet, get_ovv, vs_search
-from index import get_stop_word, build_term
+import re
+from flask import *
+from nltk.corpus import stopwords
 
-# Create an instance of the flask application within the appropriate namespace (__name__).
-# By default, the application will be listening for requests on port 5000 and assuming the base 
-# directory for the resource is the directory where this module resides.
+from index import Movie
+from pprint import pprint
+from elasticsearch_dsl import Q
+from elasticsearch_dsl.utils import AttrList
+from elasticsearch import Elasticsearch
+from elasticsearch_dsl import Search
+
 app = Flask(__name__)
 
-# Welcome page
-# Python "decorators" are used by flask to associate url routes to functions.
-# A route is the path from the base directory (as it would appear in the url)
-# This decorator ties the top level url "localhost:5000" to the query function, which
-# renders the query_page.html template.
+# Initialize global variables for rendering page
+tmp_text = ""
+tmp_title = ""
+tmp_star = ""
+tmp_min = ""
+tmp_max = ""
+tmp_director = ""
+tmp_language = ""
+tmp_country = ""
+tmp_location = ""
+tmp_time = ""
+tmp_categories = ""
+gresults = {}
+mode = "text conjunctive"
+stop_lst = stopwords.words('english')
+
+
+# display query page
 @app.route("/")
-def query():
-    """For top level route ("/"), simply present a query page."""
-    return render_template('query_page.html')
+def search():
+    return render_template('page_query.html')
 
 
-# This takes the form data produced by submitting a query page request and returns a page displaying
-# results (SERP).
-@app.route("/results", methods=['POST'])
-def results():
-    """Generate a result set for a query and present the 10 results starting with <page_num>."""
+# display results page for first set of results and "next" sets.
+@app.route("/results", defaults={'page': 1}, methods=['GET', 'POST'])
+@app.route("/results/<page>", methods=['GET', 'POST'])
+def results(page):
+    global tmp_text, phrases  # to store phrases like "philip roth"
+    global tmp_title
+    global tmp_star
+    global tmp_min
+    global tmp_max
+    global tmp_director
+    global tmp_language
+    global tmp_country
+    global tmp_location
+    global tmp_time
+    global tmp_categories
+    global gresults
+    global mode   # field to store whether we use "Conjunctive" or "Disjunctive" search for text field search
 
-    page_num = int(request.form['page_num'])
-    query = request.form['query']  # Get the raw user query
-    query_terms = build_term(query)
+    # convert the <page> parameter in url to integer.
+    if type(page) is not int:
+        page = int(page.encode('utf-8'))
+        # if the method of request is post (for initial query), store query in local global variables
+    # if the method of request is get (for "next" results), extract query contents from client's global variables  
+    if request.method == 'POST':
+        text_query = request.form['query']
+        phrases = re.findall(r"\"(.*?)\"", text_query)   # get specific phrases from query text
+        # remove all " in the query text
+        if "\"" in text_query:
+            text_query = text_query.replace("\"", "")
+        star_query = request.form['starring']
+        mintime_query = request.form['mintime']
+        if len(mintime_query) is 0:
+            mintime = 0
+        else:
+            mintime = int(mintime_query)
+        maxtime_query = request.form['maxtime']
+        if len(maxtime_query) is 0:
+            maxtime = 99999
+        else:
+            maxtime = int(maxtime_query)
+        director_query = request.form['director']
+        language_query = request.form['language']
+        country_query = request.form['country']
+        location_query = request.form['location']
+        time_query = request.form['time']
+        categories_query = request.form['categories']
+        # update global variable template data
+        tmp_text = text_query
+        tmp_star = star_query
+        tmp_min = mintime
+        tmp_max = maxtime
+        tmp_director = director_query
+        tmp_language = language_query
+        tmp_country = country_query
+        tmp_location = location_query
+        tmp_time = time_query
+        tmp_categories = categories_query
+    else:
+        # use the current values stored in global variables.
+        text_query = tmp_text
+        star_query = tmp_star
+        mintime = tmp_min
+        if tmp_min > 0:
+            mintime_query = tmp_min
+        else:
+            mintime_query = ""
+        maxtime = tmp_max
+        if tmp_max < 99999:
+            maxtime_query = tmp_max
+        else:
+            maxtime_query = ""
+        director_query = tmp_director
+        language_query = tmp_language
+        country_query = tmp_country
+        location_query = tmp_location
+        time_query = tmp_time
+        categories_query = tmp_categories
 
-    # Keep track of any stop words removed from the query to display later.
-    # Stop words should be stored in persistent storage when building your index,
-    # and loaded into your search engine application when the application is started.
-    dummy_stop_words = get_stop_word(query)
-    skipped = list(dummy_stop_words)
-    dummy_unknown_terms = get_ovv(query_terms)
-    unknown_terms = list(dummy_unknown_terms)
+    # store query values to display in search boxes in UI
+    shows = {}
+    shows['text'] = text_query
+    shows['starring'] = star_query
+    shows['director'] = director_query
+    shows['country'] = country_query
+    shows['language'] = language_query
+    shows['time'] = time_query
+    shows['location'] = location_query
+    shows['categories'] = categories_query
+    shows['maxtime'] = maxtime_query
+    shows['mintime'] = mintime_query
+
+    # Create a search object to query our index 
+    search = Search(index='sample_film_index')
+
+    # Build up your elasticsearch query in piecemeal fashion based on the user's parameters passed in.
+    # The search API is "chainable".
+    # Each call to search.query method adds criteria to our growing elasticsearch query.
+    # You will change this section based on how you want to process the query data input into your interface.
+
+    # search for runtime using a range query
+    s = search.query('range', runtime={'gte': mintime, 'lte': maxtime})
+
+    # search for matching stars
+    # You should support multiple values (list)
+    if len(star_query) > 0:
+        s = s.query('match', starring=star_query)
+
+    if len(director_query) > 0:
+        s = s.query('match', director=director_query)
+
+    if len(location_query) > 0:
+        s = s.query('match', location=location_query)
+
+    if len(language_query) > 0:
+        s = s.query('match', language=language_query)
+
+    if len(country_query) > 0:
+        s = s.query('match', country=country_query)
+
+    if len(categories_query) > 0:
+        s = s.query('match', categories=categories_query)
+    # store the temporary search result before we do text query
+    temp_s = s
+    # use "phrase" type search to find specific phrases
+    for ph in phrases:
+        s = s.query('multi_match', query=ph, type='phrase', fields=['title', 'text'], operator='and')
+        text_query = text_query.replace(ph, '')
+    # use "phrase" type search to find other single word one by one
+    for token in text_query.split():
+        s = s.query('multi_match', query=token, type='phrase', fields=['title', 'text'], operator='and')
+    text_query = text_query.strip()
+    # Conjunctive search over multiple fields (title and text) using the text_query passed in
+    if len(text_query) > 0:
+        s = s.query('multi_match', query=text_query, type='cross_fields', fields=['title', 'text'], operator='and')
+
+    # determine the subset of results to display (based on current <page> value)
+    start = 0 + (page - 1) * 10
+    end = 10 + (page - 1) * 10
+
+    # execute search and return results in specified range.
+    response = s[start:end].execute()
+    # execute temporary search without text query to compare with actual search
+    temp_response = temp_s[start:end].execute()
+    # if actual search result is 0 but temporary result before text search is not 0, we change text query to
+    # "disjunctive"
+    if response.hits.total == 0 and temp_response.hits.total != 0:
+        mode = "disjunctive"
+        s = temp_s.query('multi_match', query=text_query, type='cross_fields', fields=['title', 'text'], operator='or')
+    else:
+        mode = "conjunctive"
+
+    # highlight
+    s = s.highlight_options(pre_tags='<mark>', post_tags='</mark>')
+    s = s.highlight('text', fragment_size=999999999, number_of_fragments=1)
+    s = s.highlight('title', fragment_size=999999999, number_of_fragments=1)
+    s = s.highlight('starring', fragment_size=999999999, number_of_fragments=1)
+    s = s.highlight('country', fragment_size=999999999, number_of_fragments=1)
+    s = s.highlight('language', fragment_size=999999999, number_of_fragments=1)
+    s = s.highlight('location', fragment_size=999999999, number_of_fragments=1)
+    s = s.highlight('categories', fragment_size=999999999, number_of_fragments=1)
+    s = s.highlight('runtime', fragment_size=999999999, number_of_fragments=1)
+    s = s.highlight('director', fragment_size=999999999, number_of_fragments=1)
+
+    response = s[start:end].execute()
+    # insert data into response
+    resultList = {}
+    for hit in response.hits:
+        result = {}
+        result['score'] = hit.meta.score
+
+        if 'highlight' in hit.meta:
+            if 'title' in hit.meta.highlight:
+                result['title'] = hit.meta.highlight.title[0]
+            else:
+                result['title'] = hit.title
+
+            if 'text' in hit.meta.highlight:
+                result['text'] = hit.meta.highlight.text[0]
+            else:
+                result['text'] = hit.text
+
+            if 'starring' in hit.meta.highlight:
+                result['starring'] = hit.meta.highlight.starring[0]
+            else:
+                result['starring'] = hit.starring
+
+            if 'director' in hit.meta.highlight:
+                result['director'] = hit.meta.highlight.director[0]
+            else:
+                result['director'] = hit.director
+
+            if 'country' in hit.meta.highlight:
+                result['country'] = hit.meta.highlight.country[0]
+            else:
+                result['country'] = hit.country
+
+            if 'language' in hit.meta.highlight:
+                result['language'] = hit.meta.highlight.language[0]
+            else:
+                result['language'] = hit.language
+
+            if 'location' in hit.meta.highlight:
+                result['location'] = hit.meta.highlight.location[0]
+            else:
+                result['location'] = hit.location
+
+            if 'categories' in hit.meta.highlight:
+                result['categories'] = hit.meta.highlight.categories[0]
+            else:
+                result['categories'] = hit.categories
+
+            if 'time' in hit.meta.highlight:
+                result['time'] = hit.meta.highlight.time[0]
+            else:
+                result['time'] = hit.time
+        else:
+            result['title'] = hit.title
+            result['text'] = hit.text
+            result['starring'] = hit.starring
+            result['time'] = hit.time
+            result['categories'] = hit.categories
+            result['location'] = hit.location
+            result['language'] = hit.language
+            result['country'] = hit.country
+            result['director'] = hit.director
+
+        resultList[hit.meta.id] = result
+
+    # make the result list available globally
+    gresults = resultList
+
+    # get the total number of matching results
+    result_num = response.hits.total
+
+    # if we find the results, extract title and text information from doc_data, else do nothing
+    if result_num > 0:
+        return render_template('page_SERP.html', mode=mode, results=resultList, res_num=result_num, page_num=page, queries=shows)
+    else:
+        message = []
+        if len(text_query) > 0:
+            tokens = text_query.split()
+            unknowns = []
+            for i in range(len(tokens)):
+                if tokens[i] in stop_lst:
+                    # store the stop words in query text
+                    message.append('Contains Stop Words: ' + tokens[i] + '\n')
+                else:
+                    # store the actual unknown words in query text
+                    unknowns.append(tokens[i])
+            if len(unknowns) > 0:
+                message.append('Unknown search term: ' + ", ".join(unknowns))
+
+        if len(star_query) > 0:
+            message.append('Cannot find star: ' + star_query)
+
+        return render_template('page_SERP.html', mode=mode, results=message, res_num=result_num, page_num=page, queries=shows)
 
 
-    # If your search found any query terms that are not in the index, add them to unknown_terms and
-    # render the error_page.
-    #if unknown_terms:
-    #    return render_template('error_page.html', unknown_terms=unknown_terms)
-    #else:
-    # At this point, your query should contain normalized terms that are not stopwords or unknown.
-    boolean_ids = dummy_search(query)  # Get a list of movie doc_ids that satisfy the query.
-    vs_ids = vs_search(query, boolean_ids, 50)
-    print(vs_ids)
-    num_hits = len(vs_ids)  # Save the number of hits to display later
-    # render the results page
-    # Limit of 10 results per page
-    movie_ids = vs_ids[((page_num - 1) * 10):(page_num * 10)]
-    print(movie_ids)
-    # movie_results = list(map(dummy_movie_snippet, movie_ids))  # Get movie snippets: title, abstract, etc.
-    # Using list comprehension:
-    movie_results = [dummy_movie_snippet(fm_id) for (score, fm_id, missing_term) in movie_ids]
-    movie_results = [(fm_id, fm_title, fm_txt, movie_ids[0][0], movie_ids.pop(0)[2]) for (fm_id, fm_title, fm_txt) in movie_results]
-    print(movie_results)
-    return render_template('results_page.html', orig_query=query, movie_results=movie_results, srpn=page_num,
-                           len=len(movie_results), skipped_words=skipped, unknown_terms=unknown_terms, total_hits=num_hits)
+# display a particular document given a result number
+@app.route("/documents/<res>", methods=['GET'])
+def documents(res):
+    global gresults
+    film = gresults[res]
+    filmtitle = film['title']
+    for term in film:
+        if type(film[term]) is AttrList:
+            s = "\n"
+            for item in film[term]:
+                s += item + ",\n "
+            film[term] = s
+    # fetch the movie from the elasticsearch index using its id
+    movie = Movie.get(id=res, index='sample_film_index')
+    filmdic = movie.to_dict()
+    film['runtime'] = str(filmdic['runtime']) + " min"
+    return render_template('page_targetArticle.html', film=film, title=filmtitle)
 
 
-# Process requests for movie_data pages
-# This decorator uses a parameter in the url to indicate the doc_id of the film to be displayed
-@app.route('/movie_data/<film_id>')
-def movie_data(film_id):
-    """Given the doc_id for a film, present the title and text (optionally structured fields as well)
-    for the movie."""
-    data = dummy_movie_data(str(film_id))  # Get all of the info for a single movie
-    return render_template('doc_data_page.html', data=data)
-
-
-# If this module is called in the main namespace, invoke app.run().
-# This starts the local web service that will be listening for requests on port 5000.
 if __name__ == "__main__":
-    
-    app.run(debug=True)
-    # While you are debugging, set app.debug to True, so that the server app will reload
-    # the code whenever you make a change.  Set parameter to false (default) when you are
-    # done debugging.
-
+    app.run()
